f90c38c4837c7a42830c3507fa40254cac457574
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-06-19 01:17:59 -0300

    🎨 improving visual details in GenreListChipsWidget

--- lib/src/shared/widgets/genre_list_chips_widget.dart
+++ lib/src/shared/widgets/genre_list_chips_widget.dart
@@ -1,45 +1,52 @@
 import 'package:cinema_app/models/genres.dart';
 import 'package:cinema_app/models/movie.dart';
+import 'package:cinema_app/src/shared/controllers/genres_movies_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class GenreListChipsWidget extends StatelessWidget {
   const GenreListChipsWidget({
     Key key,
+    this.backgroundColorChip = Colors.white24,
+    this.fontColorChip = Colors.white,
     @required this.movieItem,
-    @required this.genresList,
   }) : super(key: key);
 
   final Movie movieItem;
-  final List<Genres> genresList;
+  final Color backgroundColorChip;
+  final Color fontColorChip;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return ListView.separated(
-        padding: const EdgeInsets.only(left: 20),
-        physics: const BouncingScrollPhysics(),
-        separatorBuilder: (context, i) => const SizedBox(
-              width: 10,
-            ),
-        itemCount: movieItem.genreIds.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, i) => Theme(
-              data: ThemeData(canvasColor: Colors.transparent),
-              child: Chip(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                ),
-                backgroundColor: Colors.white24,
-                label: Text(
-                  genresList
-                      .firstWhere((item) => item.id == movieItem.genreIds[i])
-                      .name,
-                  style: theme.chipTheme.labelStyle.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+      padding: const EdgeInsets.only(left: 20),
+      physics: const BouncingScrollPhysics(),
+      separatorBuilder: (context, i) => const SizedBox(
+        width: 10,
+      ),
+      itemCount: movieItem.genreIds.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, i) => Theme(
+        data: ThemeData(canvasColor: Colors.transparent),
+        child: Chip(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+          ),
+          backgroundColor: backgroundColorChip,
+          label: GetBuilder<GenresMoviesController>(builder: (genres) {
+            return Text(
+              genres.items
+                  .firstWhere((item) => item.id == movieItem.genreIds[i])
+                  .name,
+              style: theme.chipTheme.labelStyle.copyWith(
+                color: fontColorChip,
+                fontWeight: FontWeight.bold,
               ),
-            ));
+            );
+          }),
+        ),
+      ),
+    );
   }
 }
